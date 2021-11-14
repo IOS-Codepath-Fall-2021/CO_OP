118b24908d1f4ab3bf1b1d381ee328a8b3f7131d
Juntao Feng <79394873+AlvinlolZ@users.noreply.github.com>
2021-11-14 16:21:30 -0500

    Nutrition App

---
+++ Nutrition App/Nutrition App/SearchViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SearchViewController.swift
+//  Nutrition App
+//
+//  Created by Alvin Feng on 11/14/21.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
